b026a1228d1ddf3e2a4849ca606dabf98af5fb5d
Ricardo Pariz <ricardoferreirapariz@gmail.com>
2019-10-31 20:48:49 -0300

    API#003 CRUD de estacionamentos

--- src/controllers/EstacionamentoController.js
+++ src/controllers/EstacionamentoController.js
@@ -1,5 +1,76 @@
+const Estacionamento = require('../models/Estacionamento');
+const mongoose = require('mongoose');
+
 module.exports = {
-    async create(req, res){
-        return res.json({ok: true});
+    async create(req, res) {
+        try {
+            const { nome, endereco, funcionarios, dono, numeroDeVagas, vagas } = req.body;
+
+            if (!nome || !endereco || !funcionarios || !dono || !numeroDeVagas || !vagas) {
+                return res.status(500).send('Informações não enviadas para o servidor');
+            }
+
+            let estacionamento = await Estacionamento.create({
+                nome,
+                endereco,
+                funcionarios: funcionarios.map(f => new mongoose.Types.ObjectId(f)),
+                dono: new mongoose.Types.ObjectId(dono),
+                numeroDeVagas: numeroDeVagas,
+                vagas: vagas.map(v => new mongoose.Types.ObjectId(v))
+            })
+
+            estacionamento = await estacionamento.populate('funcionarios').populate('dono').execPopulate();
+
+            return res.json(estacionamento);
+        } catch (err) { res.status(500).send(err.message) }
+    },
+
+    async getAll(req, res) {
+        try {
+
+            const estacionamentos = await Estacionamento.find().populate('funcionarios').populate('dono').exec();
+            return res.json(estacionamentos);
+
+        } catch (err) { res.status(500).send(err.message) }
+    },
+
+    async getOne(req, res) {
+        try {
+            const { estacionamento } = req.params;
+
+            if (!estacionamento) {
+                return res.status(500).send('Informações não enviadas para o servidor');
+            }
+
+            const estacionamentoEncontrado = await Estacionamento.findOne({ _id: new mongoose.Types.ObjectId(estacionamento) }).populate('funcionarios').populate('dono').exec();
+            return res.json(estacionamentoEncontrado);
+
+        } catch (err) { res.status(500).send(err.message) }
+    },
+
+    async update(req, res) {
+        try {
+            const { estacionamento, nome, endereco, funcionarios, dono, numeroDeVagas, vagas } = req.body;
+
+            if (!estacionamento || !nome || !endereco || !funcionarios || !dono || !numeroDeVagas || !vagas) {
+                return res.status(500).send('Informações não enviadas para o servidor');
+            }
+
+            let estacionamentoAtualizado = await Estacionamento.findOneAndUpdate({
+                _id: new mongoose.Types.ObjectId(estacionamento)
+            }, {
+                $set: {
+                    nome,
+                    endereco,
+                    funcionarios: funcionarios.map(f => new mongoose.Types.ObjectId(f)),
+                    dono: new mongoose.Types.ObjectId(dono),
+                    numeroDeVagas: numeroDeVagas,
+                    vagas: vagas.map(v => new mongoose.Types.ObjectId(v))
+                }
+            }, { new: true }).populate('funcionarios').populate('dono').exec();
+
+
+            return res.json(estacionamentoAtualizado);
+        } catch (err) { res.status(500).send(err.message) }
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const UsuarioController = require('./controllers/UsuarioController');
+const EstacionamentoController = require('./controllers/EstacionamentoController');
 const routes = express.Router();
 
+
+// Rotas de Usuários
 routes.get('/usuarios/:usuario', UsuarioController.getOne)
 routes.route('/usuarios')
     .get(UsuarioController.getAll)
     .post(UsuarioController.create)
     .put(UsuarioController.update)
+
+
+// Rotas de Estacionamento
+routes.get('/estacionamentos/:estacionamento', EstacionamentoController.getOne)
+routes.route('/estacionamentos')
+    .get(EstacionamentoController.getAll)
+    .post(EstacionamentoController.create)
+    .put(EstacionamentoController.update)
+
+
 
 module.exports = routes;
