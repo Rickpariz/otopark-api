5956cc798f6894c2726bd36ecf8ce882c83c6cb1
Ricardo Pariz <ricardoferreirapariz@gmail.com>
2019-12-20 12:06:13 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
 
-mongoose.connect('mongodb://localhost/otopark', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
+mongoose.connect('mongodb+srv://rickpariz:rickpariz@rickpariz-xejxo.gcp.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
 
 server.use(cors({credentials: true, origin: true}));
 
 server.use(express.json());
 server.use(routes);
 
-server.listen(3003)
+server.listen(process.env.PORT || 3003);
 
